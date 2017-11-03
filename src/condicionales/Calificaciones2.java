ba32790426927f49a054c38c338b3b5454864194
jonomy <ceciliabelmar12345@gmail.com>
2017-11-03 19:12:26 -0300

    calificaciones2

---
+++ src/condicionales/Calificaciones2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionales;
+
+/**
+ *
+ * @author LAB06
+ */
+public class Calificaciones2 
+        {
+    public static void main (String[] args)
+    float (nota7);
+  float (nota6);
+  float (4<nota<5);
+  float (nota>4)
+     if     
+       nota1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese una nota"));  
+    
+       nota2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese una nota"));
+       nota3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese una nota"));
+       promedio =Float.parseFloat(JOptionPane.showInputDialog("Ingrese promedio"));
+       
+         
+            
+            
+            
+            
+            
+    
+}
